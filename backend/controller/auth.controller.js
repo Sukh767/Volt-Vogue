e48464e2004413c5c377c0e1071b9755cc120f46
Sukharanjan Jana <sukharanjanjana767@gmail.com>
2024-11-24 02:55:49 +0530

    Setup backend
    create server and add routes for login,signup and logout

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,14 @@
+
+export const signup = async(req, res) => {
+    res.send('Signup controller')
+}
+
+
+export const login = async(req, res) => {
+    res.send('Login controller')
+}
+
+
+export const logout = async(req, res) => {
+    res.send('Logout controller')
+}
---
+++ backend/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { login, logout, signup } from '../controller/auth.controller.js';
+
+
+const router = express.Router();
+
+//Auth routes
+router.get('/signup', signup)
+
+router.get('/login',login)
+
+router.get('/logout', logout)
+
+export default router
